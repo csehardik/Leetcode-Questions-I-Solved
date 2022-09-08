45fa7a4832bad905c34c5c4ac21d6bc27bda9071
Hardik Aggarwal <71537080+csehardik@users.noreply.github.com>
2022-09-08 09:48:15 +0530

    Time: 1234 ms (5.01%), Space: 250 MB (5.01%) - LeetHub

---
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        return totalWays(coins,0,amount,new HashMap<String,Integer>());
+    }
+    private int totalWays(int coins[],int currentIndex,int amount,HashMap<String,Integer>memo){
+        if(amount==0)
+            return 1;
+        if(currentIndex>=coins.length)
+            return 0;
+        
+        int currentCoin = coins[currentIndex];
+        
+        int consider = 0 ;
+        
+        String currentKey = Integer.toString(currentIndex) + '-' + Integer.toString(amount);
+        
+        if(memo.containsKey(currentKey))
+            return memo.get(currentKey);
+        
+        if(currentCoin<=amount)
+            consider = totalWays(coins,currentIndex,amount-currentCoin,memo);
+        
+        int notConsider = totalWays(coins,currentIndex+1,amount,memo);
+        
+        memo.put(currentKey,consider+notConsider);
+        return memo.get(currentKey);
+        
+    }
+}
