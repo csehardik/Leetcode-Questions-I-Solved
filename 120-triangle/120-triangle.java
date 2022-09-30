3362a44cf44193700206c8ef8a3e658a4fe95912
Hardik Aggarwal <71537080+csehardik@users.noreply.github.com>
2022-09-30 15:21:58 +0530

    Time: 20 ms (7.74%), Space: 45 MB (7.60%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,14 +1,23 @@
-// bottom-up approach code
+// top down approach / memoized code
 
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int arr[] = new int[triangle.size()+1];
-        for(int i=triangle.size()-1;i>=0;i--){
-            for(int j=0;j<triangle.get(i).size();j++){
-                arr[j] = Math.min(arr[j],arr[j+1])+triangle.get(i).get(j);
-            }
-        }
-        return arr[0];
+        return minSum(triangle,0,0,new HashMap<String,Integer>());
+    }
+public int minSum(List<List<Integer>> arr,int row,int j,HashMap<String,Integer>memo)  // [ here j = column ]
+    {
+        if(row==arr.size()-1) 
+            return arr.get(row).get(j);
+    
+    String currentKey = Integer.toString(row) + '-' + Integer.toString(j);
+    
+    if(memo.containsKey(currentKey))
+        return memo.get(currentKey);
+        
+        int down = arr.get(row).get(j) + minSum(arr,row+1,j,memo);
+        int diagonal = arr.get(row).get(j) + minSum(arr,row+1,j+1,memo);
+        
+    memo.put(currentKey,Math.min(down,diagonal));
+        return memo.get(currentKey);
     }
 }
-    
