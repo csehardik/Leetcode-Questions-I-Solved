297ea1bee0a815f6193218b72f8866e4791d50a1
Hardik Aggarwal <71537080+csehardik@users.noreply.github.com>
2023-02-19 17:46:23 +0530

    Time: 2 ms (96.72%), Space: 42.2 MB (78.80%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -7,18 +7,18 @@ class Solution {
                 memo.put(currentNum,true);
             }
         }
-        ArrayList<Integer> answer = new ArrayList<Integer>();
-        
-        for(int currentNum:nums2){
-            if(memo.containsKey(currentNum) && memo.get(currentNum)){
-                answer.add(currentNum);
-                memo.put(currentNum,false);
+            ArrayList<Integer> answer =  new ArrayList<Integer>();
+            
+            for(int currentNum:nums2){
+                if(memo.containsKey(currentNum) && memo.get(currentNum)){
+                    answer.add(currentNum);
+                    memo.put(currentNum,false);
+                }
             }
-        }
-        int result[] = new int[answer.size()];
-        for(int i=0;i<answer.size();i++){
-            result[i] = answer.get(i);
-        }
-        return result;
+            int result[] = new int[answer.size()];
+            for(int i=0;i<answer.size();i++){
+                result[i] = answer.get(i);
+            }
+            return result;
     }
 }
