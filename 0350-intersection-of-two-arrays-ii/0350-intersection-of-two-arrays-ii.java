dd081bc36d48fb33ad1261662fdb9fbc2af1c079
Hardik Aggarwal <71537080+csehardik@users.noreply.github.com>
2023-02-22 11:22:27 +0530

    Time: 4 ms (36.22%), Space: 44.7 MB (5.64%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,54 +1,31 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         
-
-        
-        //initialize new hashmap
         HashMap<Integer,Integer> memo = new HashMap<Integer,Integer>();
+        ArrayList<Integer> answer = new ArrayList<Integer>();
         
-        //iterate over nums1 
-        for(int currentNum : nums1)
-        {
-            //check if hashmap does not contains the element
-            if(!memo.containsKey(currentNum))
-            {
-                //if doesn't contains the element create an entry and mark value as true
+        for(int currentNum:nums1){
+            if(!memo.containsKey(currentNum)){
                 memo.put(currentNum,1);
             }
             else{
                 memo.put(currentNum,memo.get(currentNum)+1);
             }
         }
-
-        //create an answer arraylist of integer type
-        ArrayList<Integer> answer = new ArrayList<>();
         
-        
-        //iterate over nums2
-        for(int currentNum : nums2)
-        {
-
-            //check if hashmap contains the element from nums2 already and is the value of that entry is true 
-            if(memo.containsKey(currentNum))
-            {
-                //if entry is true then add that elemet to the answer list and mark the entry in hashmap as false for that element key
+        for(int currentNum:nums2){
+            if(memo.containsKey(currentNum)){
                 answer.add(currentNum);
                 memo.put(currentNum,memo.get(currentNum)-1);
-            
-            if(memo.get(currentNum)==0){
-                memo.remove(currentNum);
+                if(memo.get(currentNum)==0){
+                    memo.remove(currentNum);
+                }
             }
-
         }
-        }
-
-        //create a new earray resut of size equal to answer list size
-        int result[] = new int[answer.size()];
         
-        //put all the elements of answer list in the result array
-        for(int i=0;i<answer.size();i++)
-        {
-            result[i]=answer.get(i);
+        int result[] = new int[answer.size()];
+        for(int i=0; i<answer.size();i++){
+            result[i] = answer.get(i);
         }
         return result;
     }
